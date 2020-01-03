50b0a4600916c753549b0bd1f871edf83231c4f9
adiskasenda <34200173+adiskasenda@users.noreply.github.com>
2020-01-04 00:23:40 +0700

    level 2 versi 1.0.0

---
+++ Assets/Coins.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coins : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    static int coin = 0;
+     AudioClip coinSound;
+     void Start()
+     {
+         coin = 0;
+     }
+ void OnTriggerEnter2D(Collider2D other)
+     {
+         if (other.tag == "karakter")
+         {
+             coin++;    
+             AudioSource.PlayClipAtPoint(this.GetComponent<AudioSource>().clip, this.transform.position);
+             Destroy(this.gameObject);
+  
+         }
+     }
+     void OnDisable(){
+         PlayerPrefs.SetInt ("coin", coin);
+     }
+     IEnumerator Ding(){
+             yield return new WaitForSeconds (0.4F);
+        }
+}
